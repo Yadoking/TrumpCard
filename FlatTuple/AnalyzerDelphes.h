f219e6fe37463dab7f18aa7799309c261683ed01
Junghwan John Goh <jhgoh@cern.ch>
2017-07-14 03:44:40 +0900

    renovate anlayzer with new fitter

--- FlatTuple/AnalyzerDelphes.h
+++ FlatTuple/AnalyzerDelphes.h
@@ -81,7 +81,7 @@ public :
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
-   virtual void     Loop(const double hadW_m);
+   virtual void     Loop(const std::string mode, const std::string outFileName);
    virtual Bool_t   Notify();
    virtual void     Show(Long64_t entry = -1);
 };
