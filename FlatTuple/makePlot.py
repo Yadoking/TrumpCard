57cde5f4ab1b7df9ec66d7603333295d8ba0160c
Junghwan John Goh <jhgoh@cern.ch>
2017-08-07 18:18:40 +0900

    fine tune histograms

--- FlatTuple/makePlot.py
+++ FlatTuple/makePlot.py
@@ -3,7 +3,7 @@
 from ROOT import *
 from PlotMaker import *
 
-basedir = "../FlatTuple/full_shift_rotation_flip/m3"
+basedir = "ntuple"
 
 h = HistInfo()
 h.setLumi(1.0)
@@ -17,16 +17,15 @@ br_tcH = 1.00
 #h.addData("ntuple/DataSingleEG.root")
 
 ## Set MC samples
-h.addSig("tcH", "t#bar{t}, t#rightarrow{}Hc (BR=%f%%)" % br_tcH*100, "%s/delphes_FCNC.root" % basedir, kBlue, xsec_ttbar*br_tcH*br_ttLJ, 500000)
-
+h.addSig("FCNC", "FCNC", "%s/delphes_FCNC.root" % basedir, kBlue, xsec_ttbar*br_tcH*br_ttLJ, 500000)
 h.addBkg("ttbb", "t#bar{t}+b#bar{b}", "%s/delphes_ttbb.root" % basedir, kRed, xsec_ttbb*br_ttLJ, 500000)
 
 ## Define plots
-h.add1D("met_pt", "met_pt", "MET p_{T} (GeV);Events / 2GeV", 100, 0, 200)
+h.add1D("met_pt", "met_pt", "MET p_{T} (GeV);Events / 5GeV", 40, 0, 200)
 h.add1D("jets_n", "jets_n", "Jet multiplicity;Events", 10, 0, 10)
 h.add1D("bjets_n", "bjets_n", "B-jet multiplicity;Events", 10, 0, 10)
-h.add1D("hadT_m", "kin_hadT_m", "Hadronic top mass (GeV);Events / 5GeV", 100, 0, 500)
-h.add1D("hadW12_m", "kin_hadW12_m", "Hadronic W_{12} mass (GeV);Events / 3GeV", 100, 0, 300)
+h.add1D("hadT_m", "kin_hadT_m", "Hadronic top mass (GeV);Events / 10GeV", 50, 0, 500)
+h.add1D("hadW12_m", "kin_hadW12_m", "Hadronic W_{12} mass (GeV);Events / 10GeV", 50, 0, 500)
 
 ## Define cut flows
 h.addCutStep("step1", "lepton_pt>30 && abs(lepton_eta)<2.1", "lepton_pt,lepton_eta,jets_n,met_pt", "weight_gen")
