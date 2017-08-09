ae5279793f795fc8561427a873adfea7c0a23b0c
Junghwan John Goh <jhgoh@cern.ch>
2017-08-09 14:05:06 +0900

    upper case

--- FlatTuple/AnalyzeM3Delphes.h
+++ FlatTuple/AnalyzeM3Delphes.h
@@ -96,7 +96,7 @@ public :
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
-   virtual void     Loop(const std::string modeStr, const std::string outFileName, const std::string algoTypeName);
+   virtual void     Loop(const std::string modeStr, const std::string outFileName, std::string algoTypeName);
    virtual Bool_t   Notify();
    virtual void     Show(Long64_t entry = -1);
 
