cf3c6a72d0d87f6bfc01ad3e660f6cd9a82b4c4a
Junghwan John Goh <jhgoh@cern.ch>
2017-08-15 08:37:55 +0900

    Compare AUC, draw top 5 and worst 5, DNN specific plots

--- TMVA/analyzeROC.py
+++ TMVA/analyzeROC.py
@@ -14,7 +14,8 @@ gStyle.SetOptTitle(0)
 gStyle.SetOptStat(0)
 
 grps = OrderedDict()
-colors = [kBlack, kBlue, kRed, kMagenta, kGreen+1, kYellow+2, kAzure+2, kRed+4]
+colors = [kRed, kBlue+1, kGreen+3, kOrange-3, kMagenta+2]
+colors.extend([kRed-1, kBlue-1, kGreen-5, kOrange+3, kMagenta-1])
 
 files = []
 for fName in sys.argv[1:]:
@@ -22,8 +23,8 @@ for fName in sys.argv[1:]:
     if f == None: continue
     files.append(f)
 
-    for dBaseName in [key.GetName() for key in f.GetListOfKeys()]:
-        dBase = f.Get(dBaseName)
+    for dBaseName0 in [key.GetName() for key in f.GetListOfKeys()]:
+        dBase = f.Get(dBaseName0)
         for dName in [key.GetName() for key in dBase.GetListOfKeys()]:
             if dName.split('_', 1)[0] != 'Method': continue
             d = dBase.GetDirectory(dName)
@@ -34,7 +35,6 @@ for fName in sys.argv[1:]:
             hBkgEff = d.Get("%s/MVA_%s_effB" % (methodName, methodName))
 
             grp = TGraph()
-            grp.SetLineColor(colors[len(grps)-1])
             grp.SetLineWidth(2)
             for b in range(hSigEff.GetNbinsX()):
                 mvaVal = hSigEff.GetXaxis().GetBinLowEdge(b+1)
@@ -55,18 +55,25 @@ for fName in sys.argv[1:]:
                 if dx == 0: continue
                 auc += y*dx
 
+            dBaseName = dBaseName0.replace("mva_delphes", "").replace("mva_cmsTuple", "").strip("_")
             grps["%s %s" % (dBaseName, methodName)] = (grp, auc)
 grps = OrderedDict(reversed(sorted(grps.iteritems(), key=lambda x: x[1][1])))
 
 cROC = TCanvas("cROC", "cROC", 500, 500)
 hFrameROC = TH2F("hFrameROC", "hFrame;Signal efficiency;Background rejection", 100, 0, 1, 100, 0, 1)
 hFrameROC.Draw()
-leg = TLegend(0.2, 0.2, 0.5, 0.5)
+leg = TLegend(0.2, 0.15, 0.6, 0.6)
 leg.SetBorderSize(0)
-leg.SetFillStyle(0)
+leg.SetFillStyle(1111)
 cAUC = TCanvas("cAUC", "cAUC", 500, 500)
 hAUC = TH1D("hAUC", "AUC;;Area under curve", len(grps), 0, len(grps))
 hAUC.SetMinimum(0.5)
+cAUC2D = TCanvas("cAUC2D", "cAUC2D", 500, 500)
+dnnFtns, dnnNodes = ["TANH", "ReLU", "Mix"], [16,32,64,128]
+hAUC2D_DNN= TH2D("hAUC2D_DNN", "hAUC2D_DNN;Number of nodes;Number of Layers", len(dnnFtns)*len(dnnNodes), 0, len(dnnFtns)*len(dnnNodes), 8, 3, 11)
+for iw, w in enumerate(dnnFtns):
+    for jx, x in enumerate(dnnNodes):
+        hAUC2D_DNN.GetXaxis().SetBinLabel(iw*len(dnnNodes)+jx+1, "%s_%d" % (w, x))
 cBkgAtWP = TCanvas("cBkgAtWP", "cBkgAtWP", 500, 500)
 hBkgAt90 = TH1D("hBkgAt90", "90% WP;;Background rejection", len(grps), 0, len(grps))
 hBkgAt90.SetMinimum(0)
@@ -83,21 +90,54 @@ for i, (name, (grp, auc)) in enumerate(grps.iteritems()):
     hAUC.GetXaxis().SetBinLabel(i+1, name)
     hAUC.SetBinContent(i+1, auc)
 
+    if 'DNN_' in name and '_X' in name:
+        w, x, y = name.split('DNN_')[-1].split('_')
+        x, y = int(x[1:]), int(y[1:])
+        w = dnnFtns.index(w)
+        xbin = w*len(dnnNodes)+dnnNodes.index(x)+1
+        ybin = range(3,11).index(y)+1
+        
+        hAUC2D_DNN.SetBinContent(xbin, ybin, auc)
+
     hBkgAt90.GetXaxis().SetBinLabel(i+1, name)
     hBkgAt90.SetBinContent(i+1, grp.Eval(0.9))
     hBkgAt80.SetBinContent(i+1, grp.Eval(0.8))
 
-    cROC.cd()
-    grp.Draw("l")
-
+if len(grps) > 5:
+    leg.AddEntry(0, "Top 5", "p").SetMarkerColor(kWhite)
+grpsToDraw = []
+for i, (name, (grp, auc)) in enumerate(grps.iteritems()):
+    grp.SetLineColor(colors[i])
+    grpsToDraw.append(grp)
     leg.AddEntry(grp, name, "l")
+    if len(grps) > 10 and i >= 4: break
+
+if len(grps) > 10:
+    leg.AddEntry(0, "", "p").SetMarkerColor(kWhite)
+    leg.AddEntry(0, "Worst 5", "p").SetMarkerColor(kWhite)
+
+    for i, (name, (grp, auc)) in enumerate(reversed([x for x in grps.iteritems()])):
+        grp.SetLineColor(colors[i+5])
+        grpsToDraw.append(grp)
+        leg.AddEntry(grp, name, "l")
+        if i >= 4: break
 cROC.cd()
+for grp in grpsToDraw: grp.Draw("l")
+cROC.SetGridx()
+cROC.SetGridy()
 leg.Draw()
 
 cAUC.cd()
 hAUC.Draw()
 
+cAUC2D.cd()
+hAUC2D_DNN.Draw("COLZTEXT")
+
 cBkgAtWP.cd()
 hBkgAt90.Draw()
 hBkgAt80.Draw("same")
 legBkgAtWP.Draw()
+
+for c in [cROC, cAUC, cBkgAtWP]:
+    c.Modified()
+    c.Update()
