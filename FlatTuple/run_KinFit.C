d967f09ebe8dd84ee90719ed3162c91657a84bdb
Junghwan John Goh <jhgoh@cern.ch>
2017-08-15 10:21:49 +0900

    run everything

--- FlatTuple/run_KinFit.C
+++ FlatTuple/run_KinFit.C
@@ -4,7 +4,7 @@ R__LOAD_LIBRARY(TTLJKinFit.C+)
 R__LOAD_LIBRARY(AnalyzeKinFitHYTuple.C+)
 R__LOAD_LIBRARY(AnalyzeKinFitDelphes.C+)
 
-const string mode = "FCNC";
+const string mode = "FCNC"; // mode set to FCNC, require 3 b jets
 //const string mode = "tt";
 
 void run_CMSKinFit();
@@ -30,8 +30,8 @@ void run_CMSKinFit()
   AnalyzeKinFitHYTuple tFCNC(&chainFCNC);
   AnalyzeKinFitHYTuple tTTBB(&chainTTBB);
 
-  tFCNC.Loop(mode, "kin/cmsTuple_FCNC.root"); // mode set to FCNC, require 3 b jets
-  tTTBB.Loop(mode, "kin/cmsTuple_ttbb.root"); // mode set to FCNC, require 3 b jets
+  tFCNC.Loop(mode, "kin/cmsTuple_FCNC.root");
+  tTTBB.Loop(mode, "kin/cmsTuple_ttbb.root");
 }
 
 void run_DelphesKinFit()
@@ -40,11 +40,16 @@ void run_DelphesKinFit()
   chainFCNC.Add("../Delphes2Flat/ntuple_tch.root");
 
   TChain chainTTBB("tree");
-  chainTTBB.Add("../Delphes2Flat/ntuple_tt.root");
+  chainTTBB.Add("../Delphes2Flat/ntuple_ttbb.root");
+
+  TChain chainTTJJ("tree");
+  chainTTJJ.Add("../Delphes2Flat/ntuple_tt.root");
 
   AnalyzeKinFitDelphes tFCNC(&chainFCNC);
   AnalyzeKinFitDelphes tTTBB(&chainTTBB);
+  AnalyzeKinFitDelphes tTTJJ(&chainTTJJ);
 
-  tFCNC.Loop(mode, "kin/delphes_FCNC.root"); // mode set to FCNC, require 3 b jets
-  tTTBB.Loop(mode, "kin/delphes_tt.root"); // mode set to FCNC, require 3 b jets
+  tFCNC.Loop(mode, "kin/delphes_FCNC.root");
+  tTTBB.Loop(mode, "kin/delphes_ttbb.root");
+  tTTJJ.Loop(mode, "kin/delphes_tt.root");
 }
