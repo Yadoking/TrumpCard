cd6cf64382f046781aca1034c168a7548cc8ce0f
Junghwan John Goh <jhgoh@cern.ch>
2017-11-06 17:58:47 +0900

    Remove obsolete

--- FlatTuple/run_M3.C
+++ FlatTuple/run_M3.C
@@ -1,41 +1,7 @@
-#include "AnalyzeM3HYTuple.h"
-#include "AnalyzeM3Delphes.h"
-R__LOAD_LIBRARY(AnalyzeM3HYTuple.C+)
-R__LOAD_LIBRARY(AnalyzeM3Delphes.C+)
-
-const char* mode = "FCNC";
-//const string mode = "tt";
-//const char* algo = "m3";
-const char* algo = "deltaR";
-
-void run_CMSM3();
-void run_DelphesM3();
+#include "AnalyzeBasic.h"
+R__LOAD_LIBRARY(AnalyzeBasic.C+)
 
 void run_M3()
-{
-  //run_CMSM3();
-  run_DelphesM3();
-}
-
-void run_CMSM3()
-{
-  TChain chainFCNC("ttbbLepJets/tree");
-  chainFCNC.Add("/home/minerva1993/fcnc/ntuple_jw/v2/TT_AntitopLeptonicDecay_TH_1L3B_Eta_Hct.root");
-  chainFCNC.Add("/home/minerva1993/fcnc/ntuple_jw/v2/TT_TopLeptonicDecay_TH_1L3B_Eta_Hct.root");
-  ///home/minerva1993/fcnc/ntuple_jw/v2/TT_AntitopLeptonicDecay_TH_1L3B_Eta_Hut.root
-  ///home/minerva1993/fcnc/ntuple_jw/v2/TT_TopLeptonicDecay_TH_1L3B_Eta_Hut.root
-
-  TChain chainTTBB("ttbbLepJets/tree");
-  chainTTBB.Add("/home/minerva1993/fcnc/ntuple_jw/v2/TT_powheg_ttbb.root");
-
-  AnalyzeM3HYTuple tFCNC(&chainFCNC);
-  AnalyzeM3HYTuple tTTBB(&chainTTBB);
-
-  tFCNC.Loop(mode, Form("%s/cmsTuple_FCNC.root", algo), algo);
-  tTTBB.Loop(mode, Form("%s/cmsTuple_ttbb.root", algo), algo);
-}
-
-void run_DelphesM3()
 {
   TChain chainFCNC("tree");
   chainFCNC.Add("../Delphes2Flat/ntuple_tch.root");
@@ -43,14 +9,9 @@ void run_DelphesM3()
   TChain chainTTBB("tree");
   chainTTBB.Add("../Delphes2Flat/ntuple_ttbb.root");
 
-  TChain chainTTJJ("tree");
-  chainTTJJ.Add("../Delphes2Flat/ntuple_ttbb.root");
+  AnalyzeBasic tFCNC(&chainFCNC);
+  AnalyzeBasic tTTBB(&chainTTBB);
 
-  AnalyzeM3Delphes tFCNC(&chainFCNC);
-  AnalyzeM3Delphes tTTBB(&chainTTBB);
-  AnalyzeM3Delphes tTTJJ(&chainTTJJ);
-
-  tFCNC.Loop(mode, Form("%s/delphes_FCNC.root", algo), algo);
-  tTTBB.Loop(mode, Form("%s/delphes_ttbb.root", algo), algo);
-  tTTJJ.Loop(mode, Form("%s/delphes_tt.root", algo), algo);
+  tFCNC.Loop("delphes_FCNC.root");
+  tTTBB.Loop("delphes_ttbb.root");
 }
