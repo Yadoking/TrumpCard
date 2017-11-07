4ccecc61e256aef17346a634fbeffe107f5ed714
Junghwan John Goh <jhgoh@cern.ch>
2017-11-07 10:57:42 +0900

    status messages

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 if [ -d sw ]; then
-    echo "Basic software setup is already done. Skip"
+    echo "Basic software setup is already done."
+    echo "Setting up envvars..."
     source sw/python27/bin/activate
 else
+    echo "Initializing virtualenv..."
     mkdir -p sw/install
     virtualenv --system-site-packages ./sw/python27
 
+    echo "Setting up envvars..."
     source sw/python27/bin/activate
 
+    echo "Installing packages"
     pip install --upgrade pip
 
     pip install --upgrade numpy protobuf wheel 
@@ -16,11 +20,13 @@ else
     pip install graphviz
 
     ## Install TensorFlow
+    echo "Installing TensorFlow..."
     if [ -f /share/apps/tensorflow-1.3.0-cp27-cp27mu-linux_x86_64.whl ]; then
         pip install /share/apps/tensorflow-1.3.0-cp27-cp27mu-linux_x86_64.whl 
     fi
 
     ## Install mxnet
+    echo "Installing mxnet..."
     cd sw/install
     git clone https://github.com/dmlc/mxnet --branch 0.12.0 --recursive
     cd mxnet
@@ -36,6 +42,7 @@ else
 
     cd ../../../..
 
+    echo "Installing other packages..."
     pip install pydot keras 
     pip install -U scikit-learn
 fi
